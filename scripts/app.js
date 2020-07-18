7c5da466b3c77edc7a5b5b221bac3344a925b61f
NoaFilosof <noafilosof@hotmail.co.uk>
2020-07-18 13:38:48 +0100

    JS plan for structure of JS laid oout in comments

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+// CREATE AN OUTLINE  - this will encompass all the different needs of the JS: 
+  // DOM elements: what is to be caputered from HTML
+  // GIRD variables: what will reference the grid: declare these so that they can be changed further down the line if needed
+  // GAME variables: moving elements on the screen e.g. aliens will be declared here
+  // Functions: both event functions and logic functions here; first one to create will be the button click (to start the game) and creation of the grid 
+    // button click: this will set off a timer and 'start' the game (start game function first will work on timer later)
+    // create grid: use the grid variables to create a for loop which will append the grid to the child (cells) and also push into an array - this will also greate an element called 'div' which will be styled in css for sizing
+  // EVENT listeners - make most of these later - but these will listen out for start button, and key movements on keyboard (space, left/right arrows)
+
+function init() {
+  // * DOM ELEMENTS
+
+
+  // * GRID VARIABLES
+
+
+  // * GAME VARIABLES 
+
+
+  // * FUNCTIONS 
+
+
+  // * EVENT LISTENERS
+
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
