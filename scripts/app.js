c95d7c9bee819d4c6ed84c72dee31220f1c1b6d1
NoaFilosof <noafilosof@hotmail.co.uk>
2020-07-20 09:20:39 +0100

    initial function of aliens moving down the page on a timer

--- scripts/app.js
+++ scripts/app.js
@@ -19,11 +19,19 @@ function init() {
 
   // * GAME VARIABLES 
   let ruPosition = 90
-  let michellePosition = 20
-  let rossPosition = 30
-  let carsonPosition = 40
-  let shangelaPosition = null
+  // const aliens = [1, 2, 3, 4, 5, 6, 7, 8]
+  // let aliensPosition = 3
+  // let aliens = []
+  // let createAlien = createAliens()
+  let michellePosition = width + 10
+  // let rossPosition = 30
+  // let carsonPosition = 40
+  // let shangelaPosition = null
 
+  // function createAlien() {
+  //   aliens.forEach(alien => cells[aliensPosition].classList.add('alien'))
+  // }
+  // createAlien()
 
   // * FUNCTIONS 
   // create a for loop to create 100 cells which will thus create the grid - when this happens - create an element called div (so this will happen each time with the loop), each loop, push the div (just created) into an array of cells, and also append the cell (div just created) to the parent grid.
@@ -37,8 +45,9 @@ function init() {
     }
     cells[ruPosition].classList.add('rupaul')
     cells[michellePosition].classList.add('michelle')
-    cells[rossPosition].classList.add('ross')
-    cells[carsonPosition].classList.add('carson')
+    // cells[aliensPosition].classList.add('alien')
+    // cells[rossPosition].classList.add('ross')
+    // cells[carsonPosition].classList.add('carson')
   }
   createGrid()
 
@@ -70,19 +79,31 @@ function init() {
     cells[ruPosition].classList.add('rupaul')
   } 
 
+  // create a function which hosts all the 'aliens' and duplicates them on the page 
+  // function createAliens() {
+  //   const names = ['michelle', 'carson', 'ross']
+  //   for (let names = 0; names < (width * 3); names++)
+  //     aliens.push(names)
+  //   return names
+  // }
+
   // create a function which starts the game and initiates a timer of how long the game will last until you are 'killed'
+  // write a second timer function which removes michelle/carson/ross from their places and pushes them down the grid one row after 10 seconds 
   function startGame() {
     console.log('clicked')
     let timerID = null
     let count = 0
     timerID = setInterval(() => {
-      count++
-      if (count > 4) {
+      if (count > 3) {
         clearInterval(timerID)
+        cells[michellePosition].classList.remove('michelle')
         window.alert('game over')
-      } else {
-        console.log(count)
-      }
+      } 
+      count++
+      cells[michellePosition].classList.remove('michelle')
+      michellePosition += width 
+      console.log(michellePosition)
+      cells[michellePosition].classList.add('michelle')
     }, 1000)
   }
 
