6edc2c77f7c3470e2d0c03f95a1d7615149ce9da
NoaFilosof <noafilosof@hotmail.co.uk>
2020-07-21 12:20:53 +0100

    laser shooting from above player and also eliminatinf aliens at the same time

--- scripts/app.js
+++ scripts/app.js
@@ -20,7 +20,7 @@ function init() {
 
   // * GAME VARIABLES 
   let ruPosition = 217
-  let michellePosition = width
+  let michellePosition = 0 
   const michelleStart = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 30, 31, 32,33, 34, 35 ,36, 37, 38, 39, 40, 41, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56]
   let laserPosition = ruPosition - width
   let timerID = null
